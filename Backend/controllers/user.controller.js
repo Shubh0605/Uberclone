b9746adf74c466c3b73150b554297be1e5312747
shubh012 <shubprajapati012@gmail.com>
2024-12-19 23:41:47 +0530

    first commit

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,42 @@
+const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+const { validationResult } = require('express-validator');
+
+
+module.exports.registerUser = async(req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+    }
+
+    console.log(req.body);
+
+    
+
+    
+
+    const{ fullname, email, password } = req.body;
+    const {firstname,lastname} = fullname;
+
+    const hashedPassword = await userModel.hashPassword(password);
+
+    try{
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword
+    });
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ token, user });
+    }catch (error) {
+        next(error);
+    }
+   
+
+
+
+};
